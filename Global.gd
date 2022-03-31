c8af48f61b8acb4ba94f53a565660ba21f978b6f
amontal15 <101257259+amontal15@users.noreply.github.com>
2022-03-31 08:37:46 +0200

    joc acabat
    bug fixes

--- Global.gd
+++ Global.gd
@@ -2,3 +2,6 @@ extends Node
 
 var puntuacio1 = 0
 var puntuacio2 = 0
+
+var punts = 0
+
--- escenes/rigid body_ cos2.gd
+++ escenes/rigid body_ cos2.gd
@@ -4,8 +4,8 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var puntuacio1 =0
-var puntuacio2 =0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_weight(3.0)
@@ -17,5 +17,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if position.y >= 400:
-	get_tree().change_scene("res://escena 2.tscn")
+	if position.y >= 450:
+		Global.punts +=1
+		get_tree().change_scene("res://escenes/pantalla cossos.tscn")
+	if Global.punts >= 3:
+		get_tree().change_scene("res://escena_3_gg.tscn")
+
